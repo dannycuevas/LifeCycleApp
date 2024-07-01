314aa0086fae046f97aaebbd1160bcca8b2e5f96
Daniel Cuevas <dannycuevax@hotmail.com>
2024-06-30 21:33:26 -0600

    add Class Library Project
    adds the class library project with the SQL load class and the calculator class for future use

---
+++ LifecycleLibrary/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifecycleLibrary
+{
+    public static class Calculator
+    {
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
